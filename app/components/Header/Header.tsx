901e54934c4d7864f9ccf858b9a4e5b31064b9b7
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2026-01-27 21:46:09 +0100

    create mobile menu component
    contain with Nav and AuthNav components

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import css from "./Header.module.css";
 import Nav from "../Nav/Nav";
 import Container from "../Container/Container";
 import Link from "next/link";
 import AuthNav from "../AuthNav/AuthNav";
+import { useState, useEffect } from "react";
+import MobileMenu from "../MobileMenu/MobileMenu";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "auto";
+  }, [menuOpen]);
   return (
     <header>
       <Container>
@@ -15,9 +23,22 @@ export default function Header() {
             </svg>
           </Link>
         </div>
-
         <Nav />
         <AuthNav />
+        <div className={css.mobileMenu}>
+          <button
+            className={css.mobMenuOpenBtn}
+            type="submit"
+            onClick={() => setMenuOpen(true)}
+          >
+            <svg className={css.logoIcon} width="105" height="28">
+              <use href="/symbol-defs.svg#icon-burger-btn"></use>
+            </svg>
+          </button>
+        </div>
+        {menuOpen && (
+          <MobileMenu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
+        )}
       </Container>
     </header>
   );
