21d130220531e3d8d63e7045be0a6e6bc9f0f89e
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2026-02-01 10:25:38 +0100

    update imgs for news list

--- app/components/NewsList/NewsList.tsx
+++ app/components/NewsList/NewsList.tsx
@@ -7,7 +7,7 @@ type Props = { news: New[] };
 const NewsList = ({ news }: Props) => {
   return (
     <section className={css.newsListContainer}>
-      <ul className={css.newsListContainer}>
+      <ul className={css.newsList}>
         {news.map((news) => (
           <NewsItem key={news.id} item={news} />
         ))}
