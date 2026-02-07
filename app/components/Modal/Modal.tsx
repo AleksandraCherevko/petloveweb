6ede51c827f1170ac9ddc20f0ce0edbfabd7f6fe
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2026-02-07 21:32:56 +0100

    create modal and modal attention component

---
+++ app/components/Modal/Modal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { createPortal } from "react-dom";
+import css from "./Modal.module.css";
+import { useEffect } from "react";
+
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export default function Modal({ onClose, children }: ModalProps) {
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "";
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div
+      className={css.modalOverlay}
+      onClick={handleBackdropClick}
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className={css.modalContent}>
+        <button
+          className={css.closeBtn}
+          onClick={onClose}
+          aria-label="Close modal"
+        >
+          <svg width="32" height="32" className={css.modalCloseBtnIcon}>
+            <use href="/symbol-defs.svg#icon-cross-black"></use>
+          </svg>
+        </button>
+        {children}
+      </div>
+    </div>,
+    document.body,
+  );
+}
