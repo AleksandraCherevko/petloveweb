3f4b965e1e82ba5a41f88ccfee924016c15980f2
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2026-02-10 22:18:32 +0100

    add logout

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,25 +1,12 @@
-// app/api/auth/logout/route.ts
-
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { api } from "@/app/lib/api";
 
 export async function POST() {
-  // Передаємо поточні cookie до API
-  const cookieStore = await cookies();
-  await api.post(
-    "/users/signout",
-    {},
-    {
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
-    },
-  );
-
-  // Очищаємо токени після запиту
-  cookieStore.delete("accessToken");
-  cookieStore.delete("refreshToken");
-
-  return NextResponse.json({ message: "Logged out successfully" });
+  try {
+    const response = NextResponse.json({ message: "Logged out successfully" });
+    response.cookies.delete("accessToken");
+    return response;
+  } catch (err) {
+    console.error("Logout error:", err);
+    return NextResponse.json({ error: "Failed to logout" }, { status: 500 });
+  }
 }
